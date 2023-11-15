2cc74bdd81cdfffa2fc1f0b161579bce39ce3689
Jan Siwy <82534393+kotek112@users.noreply.github.com>
2023-11-15 16:31:54 +0100

    20231115_Tablice
    Tablice wstęp

---
+++ kartkowka_1/kartkowka_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tablica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tabA = CreateArray("tabA");
+
+
+            Console.ReadKey();
+
+            
+        }
+
+        public static int[] CreateArray(string name)
+        {
+            Console.WriteLine($"podaj rozmiar tablicy {name}");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            return array;
+        }
+
+       
+    }
+
+}
